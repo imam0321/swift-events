5ed6f5d26a8207458ee79135a5182a48adfe7af3
imam0321 <imam.hossain0321@gmail.com>
2025-03-10 00:43:03 +0600

    update EventCard and ActionButtons components; integrate event data and improve layout

--- components/EventCard/EventCard.jsx
+++ components/EventCard/EventCard.jsx
@@ -1,24 +1,24 @@
 import Image from 'next/image'
 import ActionButtons from '../Buttons/ActionButtons'
 
-export default function EventCard() {
+export default function EventCard({ event }) {
   return (
-    <div>
-      <div className="overflow-hidden rounded-md bg-[#242526]">
-        <Image
-          src="/google-io-2023-1.png"
-          alt="Event 1"
-          className="w-full"
-          height={500}
-          width={500}
-        />
+    <div className=''>
+      <div className="overflow-hidden rounded-md bg-[#242526] h-[400px]">
+        <div className="h-[230px] relative">
+          <Image
+            src={event?.imageUrl}
+            fill
+            alt={event?.name || "Event Image"}
+          />
+        </div>
         <div className="p-3">
-          <a href="/details/1" className="font-bold text-lg">Google I/O Extended</a>
-          <p className="text-[#9C9C9C] text-sm mt-1">Rangpur, Dhaka, Bangladesh, Rangpur, Bangladesh</p>
+          <a href={`/details/${event?._id}`} className="font-bold text-lg">{event?.name}</a>
+          <p className="text-[#9C9C9C] text-sm mt-1">{event?.location}</p>
           <div className="text-[#737373] text-sm mt-1">
-            <span>1k Interested</span>
+            <span>{event?.interested_ids?.length || 0} Interested </span>
             <span>|</span>
-            <span>40K Going</span>
+            <span> {event?.going_ids?.length || 0} Going</span>
           </div>
           <ActionButtons />
         </div>
